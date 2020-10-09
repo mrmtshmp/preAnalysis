d6014026ffc08a842f8baba817cc9f3b97730357
mrmtshmp <mr.mt.shm@gmail.com>
2020-10-09 15:29:15 +0900

    first commit

---
+++ src/R/sub/setting_200917.R
@@ -0,0 +1,12 @@
+#' Settings
+#' proj. TNFaiHTLV1
+#' PI Dr.Endoh
+#' 2020/09/17
+
+dir.data <- "../../mail/2010"
+fn.data  <- "HTLV-1_20201006.xlsx"
+
+dir.ADS <- "../data"
+fn.ADS <- "ADS.RData"
+
+dir.output <- "./output"
